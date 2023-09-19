1db7668d256a14f65fe841bd1a94434e380e5eaa
marvelkel17 <77815307+marvelkel17@users.noreply.github.com>
2023-09-18 21:56:06 -0500

    Update MainPage.xaml.cs

--- facade/MainPage.xaml.cs
+++ facade/MainPage.xaml.cs
@@ -1,25 +1,25 @@
-﻿using System;
+using System;
 
 namespace facade;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public bool DidWin { get; set; } = false;
+    public bool DidWin { get; set; } = false;
 
-	public MainPage()
-	{
-		InitializeComponent();
-
-		BindingContext = new MainPageViewModel();
-
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
     async void Button_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync($"{nameof(GameOverPage)}?DidWin={DidWin}");
     }
+
+    async void YouWon(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"{nameof(YouWonPage)}?DidWin={DidWin}");
+    }
 }
-
-
